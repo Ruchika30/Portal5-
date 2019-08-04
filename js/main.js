7d95970683b6bbbf84626f4473a158ad1ec69fa8
Ruchika30 <sood.ruchika@siesgst.ac.in>
2019-08-05 01:06:10 +0530

    Final changes done

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ hamburger.addEventListener("click", function(e) {
   toggle(this, "open");
   //   toggle(menuItem, "hamburgeropen");
 });
+
+/* -------Visible if scrolled to 300px  -----------*/
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+//  scroll to the top of the document when clicked
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
